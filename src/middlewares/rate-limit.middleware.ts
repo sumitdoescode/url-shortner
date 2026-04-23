9ba5522de0befbf4976573db173722dff3ad8e88
sumitdoescode <sumit.does.code@gmail.com>
2026-04-23 17:51:23 +0530

    feat: implement rate limiting middleware for authentication and redirect routes

--- src/middlewares/rate-limit.middleware.ts
+++ src/middlewares/rate-limit.middleware.ts
@@ -2,7 +2,7 @@ import { rateLimiter } from "hono-rate-limiter";
 
 // for authentication we are using strict rate limits
 export const authRateLimit = rateLimiter({
-    windowMs: 10 * 60 * 1000, // 10 minutes
+    windowMs: 5 * 60 * 1000, // 5 minutes
     limit: 10, // limit each IP to 10 requests per windowMs
     keyGenerator: (c) => {
         const forwardedFor = c.req.header("x-forwarded-for");
